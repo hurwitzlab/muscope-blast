78a21a87ad9cff1e679bd47ef1690a7ae6b55660
Joshua Lynch <jklynch@email.arizona.edu>
2017-02-22 10:45:23 +0000

    improved some variable names to reflect sample identity (maybe)

--- stampede/scripts/extractseqs.py
+++ stampede/scripts/extractseqs.py
@@ -4,7 +4,7 @@ extractseqs.py
 
 This script is part of the muSCOPE-BLAST CyVerse application.
 
-Read BLAST output file to get sequence hit ids for the reference database. Extract the reference database
+Read a BLAST output file to get sequence hit ids for the reference sequences. Extract the reference
 sequences corresponding to each hit and write them to a file.
 
 The muSCOPE-BLAST application generates BLAST output files with names like
@@ -21,8 +21,8 @@ Each .tab file looks like this:
     A HOT238_1c_0200m_rep_c260499_1   100.00  147 0   0   1   147 400 546 2e-70    272
     A HOT236_1_0200m_c24599_1 100.00  147 0   0   1   147 358 504 2e-70    272
 
-The second column is parsed to extract the reference database name (e.g. `HOT234_1_0200m`) and the reference
-database sequence id (e.g. `rep_c55158_2`). All matched sequences are extracted from the reference databases and
+The second column is parsed to extract the reference sample name (e.g. `HOT234_1_0200m`) and the reference
+sequence id (e.g. `rep_c55158_2`). All matched sequences are extracted from the reference sample FASTA files and
 written to a file.
 """
 
@@ -58,33 +58,35 @@ def main(blast_output_fp, ohana_sequence_dp, ohana_hit_output_dp, blast_hit_limi
 
     # parse the BLAST output file
     with open(blast_output_fp, 'rt') as blast_output_file:
-        blast_db_hits = get_blast_reference_hits(blast_output_file=blast_output_file, blast_hit_limit=blast_hit_limit)
+        blast_refdb_hits = get_blast_reference_hits(blast_output_file=blast_output_file, blast_hit_limit=blast_hit_limit)
     print('finished parsing BLAST output file "{}"'.format(blast_output_fp))
 
     # set up file paths
     blast_input_file_name, seq_type = parse_blast_output_file_path(blast_output_fp)
     blast_input_base_name = os.path.basename(blast_input_file_name)
     seq_type_ext = {'contigs': '.fa', 'genes': '.fna', 'proteins': '.faa'}
-    blast_db_fasta_file_pattern = os.path.join(
+    blast_ref_sample_fasta_file_pattern = os.path.join(
         ohana_sequence_dp,
-        '{refdb}',
+        '{refsample}',
         seq_type + seq_type_ext[seq_type]
     )
     output_file_pattern = os.path.join(
         ohana_hit_output_dp,
-        blast_input_base_name + '-{refdb}-' + seq_type + seq_type_ext[seq_type]
+        blast_input_base_name + '-{refsample}-' + seq_type + seq_type_ext[seq_type]
     )
 
-    # find each hit from the BLAST output in the corresponding BLAST reference database and copy the sequence to a file
-    for blast_db_name, matched_sequence_ids in sorted(blast_db_hits.items()):
-        blast_db_fasta_fp = blast_db_fasta_file_pattern.format(refdb=blast_db_name)
-        if not os.path.exists(blast_db_fasta_fp):
-            print('ERROR: BLAST database "{}" does not exist'.format(blast_db_fasta_fp))
+    # find each hit from the BLAST output in the corresponding BLAST reference sample FASTA files
+    # and copy the sequence to a file
+    for blast_ref_sample_name, matched_sequence_ids in sorted(blast_refdb_hits.items()):
+        blast_ref_sample_fasta_fp = blast_ref_sample_fasta_file_pattern.format(refsample=blast_ref_sample_name)
+        if not os.path.exists(blast_ref_sample_fasta_fp):
+            print('ERROR: BLAST database "{}" does not exist'.format(blast_ref_sample_fasta_fp))
         else:
-            output_fp = output_file_pattern.format(refdb=blast_db_name)
-            print('extracting sequence hits from "{}"'.format(blast_db_fasta_fp))
-            with open(blast_db_fasta_fp, 'rt') as blast_db_fasta_file, open(output_fp, 'wt') as output_file:
-                write_sequence_hits(matched_sequence_ids, blast_db_fasta_file, output_file)
+            output_fp = output_file_pattern.format(refsample=blast_ref_sample_name)
+            print('extracting sequence hits from "{}"'.format(blast_ref_sample_fasta_fp))
+            with open(blast_ref_sample_fasta_fp, 'rt') as blast_ref_sample_fasta_file, \
+                    open(output_fp, 'wt') as ref_sample_sequence_output_file:
+                write_sequence_hits(matched_sequence_ids, blast_ref_sample_fasta_file, ref_sample_sequence_output_file)
 
 
 def parse_blast_output_file_path(fp):
@@ -94,6 +96,9 @@ def parse_blast_output_file_path(fp):
         test.fa-contigs.tab
         test.fa-genes.tab
         test.fa-proteins.tab
+
+    The input file name for the examples above is 'test.fa'. The sequence types are 'contigs', 'genes' and
+    'proteins' respectively.
 
     :param fp: (str) file path or file name
     :return: 2-ple of strs (input file name, sequence type)
@@ -160,6 +165,11 @@ def get_blast_reference_hits(blast_output_file, blast_hit_limit):
 
 
 def extract_blast_db_and_sequence_id(blast_output_file):
+    """Parse and yield tuples of (sample name, sequence id) from each line of the blast_output_file.
+
+    :param blast_output_file: (file-like object)
+    :return: (sample name, sequence id)
+    """
     p = re.compile(r'(?P<db>HOT\d+(_\dc?)?_\d+m)_(?P<seq_id>(rep_)?c\d+(_\d)?)')
     for i, line in enumerate(blast_output_file):
         row_values = line.strip().split('\t')
@@ -172,7 +182,7 @@ def extract_blast_db_and_sequence_id(blast_output_file):
     print('finished parsing {} rows of BLAST output'.format(i+1))
 
 
-if __name__ == '__main__':
+def get_args():
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('blast_output_fp', metavar='FILE', help='file of BLAST hits')
     arg_parser.add_argument('ohana_sequence_dp', metavar='DIR', help='directory of Ohana contigs, genes, proteins')
@@ -181,8 +191,12 @@ if __name__ == '__main__':
         '-l', '--blast-hit-limit', type=int, default=None,  help='extract only the first <blast-hit-limit> BLAST hits')
     args = arg_parser.parse_args()
     print(args)
+    return args
 
+
+if __name__ == '__main__':
     #test_blast_output_dp = '/home/jklynch/host/project/muscope/apps/test-blast-output/'
     #test_blast_db_fasta_dp = '/home/jklynch/host/project/muscope/ohana/'
     #test_sequence_output_dp = '/home/jklynch/host/project/muscope/apps/test-sequence_output/'
+    args = get_args()
     main(**args.__dict__)
