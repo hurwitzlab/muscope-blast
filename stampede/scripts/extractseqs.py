ce0bcc4c2419f8219c8e147738da88a109e830f1
Joshua Lynch <jklynch@email.arizona.edu>
2017-02-23 21:02:27 +0000

    fixed a bug related to sequence ids

--- stampede/scripts/extractseqs.py
+++ stampede/scripts/extractseqs.py
@@ -5,7 +5,7 @@ extractseqs.py
 This script is part of the muSCOPE-BLAST CyVerse application.
 
 Read a BLAST output file to get sequence hit ids for the reference sequences. Extract the reference
-sequences corresponding to each hit and write them to a file.
+sequences corresponding to each hit from the original reference file(s) and write them to a file.
 
 The muSCOPE-BLAST application generates BLAST output files with names like
      + test.fa-contigs.tab
@@ -21,16 +21,14 @@ Each .tab file looks like this:
     A HOT238_1c_0200m_rep_c260499_1   100.00  147 0   0   1   147 400 546 2e-70    272
     A HOT236_1_0200m_c24599_1 100.00  147 0   0   1   147 358 504 2e-70    272
 
-The second column is parsed to extract the reference sample name (e.g. `HOT234_1_0200m`) and the reference
-sequence id (e.g. `rep_c55158_2`). All matched sequences are extracted from the reference sample FASTA files and
-written to a file.
+The second column is parsed to extract the reference sample name from the sequence id (e.g. `HOT234_1_0200m`). All
+matched sequences are extracted from the reference sample FASTA files and written to a file.
 """
 
 import argparse
 import collections
 import itertools
 import os
-import pprint
 import re
 
 from Bio import SeqIO
@@ -56,85 +54,69 @@ def main(blast_output_fp, ohana_sequence_dp, ohana_hit_output_dp, blast_hit_limi
         print('Sequence output directory "{}" will be created.'.format(ohana_hit_output_dp))
         os.makedirs(ohana_hit_output_dp, exist_ok=True)
 
-    # parse the BLAST output file
-    with open(blast_output_fp, 'rt') as blast_output_file:
-        blast_refdb_hits = get_blast_reference_hits(blast_output_file=blast_output_file, blast_hit_limit=blast_hit_limit)
-    print('finished parsing BLAST output file "{}"'.format(blast_output_fp))
-
     # set up file paths
-    blast_input_file_name, seq_type = parse_blast_output_file_path(blast_output_fp)
+    blast_output_filename_pattern = re.compile(r'(?P<input>.+)-(?P<seq_type>(contigs|genes|proteins))\.tab')
+    try:
+        blast_input_file_name, seq_type = blast_output_filename_pattern.search(blast_output_fp).groups()
+    except Exception as e:
+        print('  failed to parse BLAST output file name "{}"'.format(blast_output_fp))
+        raise e
+
     blast_input_base_name = os.path.basename(blast_input_file_name)
     seq_type_ext = {'contigs': '.fa', 'genes': '.fna', 'proteins': '.faa'}
-    blast_ref_sample_fasta_file_pattern = os.path.join(
+    blast_ref_sample_fasta_file_template = os.path.join(
         ohana_sequence_dp,
         '{refsample}',
         seq_type + seq_type_ext[seq_type]
     )
-    output_file_pattern = os.path.join(
+    output_file_path_template = os.path.join(
         ohana_hit_output_dp,
         blast_input_base_name + '-{refsample}-' + seq_type + seq_type_ext[seq_type]
     )
 
+    # parse the BLAST output file
+    with open(blast_output_fp, 'rt') as blast_output_file:
+        blast_refdb_hits = get_blast_reference_hits(blast_output_file=blast_output_file, blast_output_row_limit=blast_hit_limit)
+    print('finished parsing BLAST output file "{}"'.format(blast_output_fp))
+
     # find each hit from the BLAST output in the corresponding BLAST reference sample FASTA files
     # and copy the sequence to a file
-    for blast_ref_sample_name, matched_sequence_ids in sorted(blast_refdb_hits.items()):
-        blast_ref_sample_fasta_fp = blast_ref_sample_fasta_file_pattern.format(refsample=blast_ref_sample_name)
+    for blast_ref_sample_name, sample_sequence_ids in sorted(blast_refdb_hits.items()):
+        blast_ref_sample_fasta_fp = blast_ref_sample_fasta_file_template.format(refsample=blast_ref_sample_name)
         if not os.path.exists(blast_ref_sample_fasta_fp):
-            print('ERROR: BLAST database "{}" does not exist'.format(blast_ref_sample_fasta_fp))
+            print('ERROR: BLAST reference "{}" does not exist'.format(blast_ref_sample_fasta_fp))
         else:
-            output_fp = output_file_pattern.format(refsample=blast_ref_sample_name)
             print('extracting sequence hits from "{}"'.format(blast_ref_sample_fasta_fp))
-            with open(blast_ref_sample_fasta_fp, 'rt') as blast_ref_sample_fasta_file, \
-                    open(output_fp, 'wt') as ref_sample_sequence_output_file:
-                write_sequence_hits(matched_sequence_ids, blast_ref_sample_fasta_file, ref_sample_sequence_output_file)
+            output_fp = output_file_path_template.format(refsample=blast_ref_sample_name)
+            with open(blast_ref_sample_fasta_fp, 'rt') as sample_fasta_file, \
+                    open(output_fp, 'wt') as sample_sequence_output_file:
+
+                for i, seq in enumerate(find_sequences(sample_sequence_ids, sample_fasta_file)):
+                    SeqIO.write(seq, sample_sequence_output_file, 'fasta')
+
+                print('wrote {} sequence(s) to file "{}"'.format(i+1, output_fp))
 
 
-def parse_blast_output_file_path(fp):
-    """Parse input file name and sequence type (contigs, genes, proteins) from a file path.
+def find_sequences(sequence_ids, fasta_file):
+    """Search fasta_file for sequence ids in sequence_ids and write the corresponding sequence and yield each
+    sequence record that is found.
 
-    Valid file names look like this:
-        test.fa-contigs.tab
-        test.fa-genes.tab
-        test.fa-proteins.tab
-
-    The input file name for the examples above is 'test.fa'. The sequence types are 'contigs', 'genes' and
-    'proteins' respectively.
-
-    :param fp: (str) file path or file name
-    :return: 2-ple of strs (input file name, sequence type)
+    :param sequence_ids: (set of str)
+    :param fasta_file: (file-like object)
+    :yield: (biopython sequence object)
     """
-    blast_output_filename_pattern = re.compile(r'(?P<input>.+)-(?P<seq_type>(contigs|genes|proteins))\.tab')
-    m = blast_output_filename_pattern.search(fp)
-    if m is None:
-        print('  failed to parse "{}"'.format(fp))
-        return None, None
-    else:
-        return m.group('input'), m.group('seq_type')
+    remaining_sequence_ids = set(sequence_ids)
+    for seq in SeqIO.parse(fasta_file, 'fasta'):
+        if seq.id in remaining_sequence_ids:
+            remaining_sequence_ids.remove(seq.id)
+            yield seq
 
-
-def write_sequence_hits(matched_sequence_ids, blast_db_fasta_file, output_file):
-    """Search blast_db_fasta_file for sequence ids in matched_sequence_ids and write the corresponding sequence
-    to output_file.
-
-    :param matched_sequence_ids: (set of str)
-    :param blast_db_fasta_file: (file-like object)
-    :param output_file: (file-like object)
-    :return: no return value
-    """
-    blast_hit_id_search_set = set(matched_sequence_ids)
-    for seq in SeqIO.parse(blast_db_fasta_file, 'fasta'):
-        if seq.id in blast_hit_id_search_set:
-            # print(seq)
-            SeqIO.write(seq, output_file, 'fasta')
-            blast_hit_id_search_set.remove(seq.id)
-
-            if len(blast_hit_id_search_set) == 0:
+            if len(remaining_sequence_ids) == 0:
                 break
-    for seq_id in blast_hit_id_search_set:
-                print('  ERROR: failed to find "{}"'.format(seq_id))
+    return
 
 
-def get_blast_reference_hits(blast_output_file, blast_hit_limit):
+def get_blast_reference_hits(blast_output_file, blast_output_row_limit):
     """Read the specified BLAST output file to build a dictionary with Ohana reference database names as keys and
     sets of sequence ids as values.
 
@@ -146,40 +128,31 @@ def get_blast_reference_hits(blast_output_file, blast_hit_limit):
 
     The dictionary generated by this function looks like this:
         {
-            'HOT234_1_0200m': {'rep_c55158_2', 'c10096_4'},
-            'HOT238_1c_0200m': {'c3_1', 'rep_c260499_1'}
+            'HOT234_1_0200m': {'HOT234_1_0200m_rep_c55158_2', 'HOT234_1_0200m_c10096_4'},
+            'HOT238_1c_0200m': {'HOT238_1c_0200m_c3_1', 'HOT238_1c_0200m_rep_c260499_1'}
         }
 
     :param blast_output_file: (file-like object) BLAST output file
-    :param blast_hit_limit: (int) maximum number of BLAST hits to process, None to process all hits
+    :param blast_output_row_limit: (int) maximum number of BLAST output rows to process, None to process all rows
     :return: dictionary of Ohana reference database names mapped to sets of sequence ids
     """
     blast_db_hits = collections.defaultdict(set)
 
-    for blast_hit_id, ohana_blast_db, seq_id \
-            in itertools.islice(extract_blast_db_and_sequence_id(blast_output_file), blast_hit_limit):
-        blast_db_hits[ohana_blast_db].add(seq_id)
+    sample_sequence_pattern = re.compile(r'^(?P<sample_id>HOT\d+_(\d*c?_)?\d+m)')
+    for i, blast_output_row in enumerate(itertools.islice(blast_output_file, blast_output_row_limit)):
+        print(blast_output_row)
+        try:
+            _, sample_sequence, _ = blast_output_row.strip().split('\t', maxsplit=2)
+            sample_id = sample_sequence_pattern.match(sample_sequence).group('sample_id')
+            blast_db_hits[sample_id].add(sample_sequence)
+        except Exception as e:
+            print('failed to parse row {}: "{}"'.format(i+1, blast_output_row))
+            raise e
 
-    pprint.pprint(blast_db_hits)
+    row_count = sum([len(sequences) for sequences in blast_db_hits.values()])
+    print('finished parsing {} rows of BLAST output'.format(row_count))
+
     return blast_db_hits
-
-
-def extract_blast_db_and_sequence_id(blast_output_file):
-    """Parse and yield tuples of (sample name, sequence id) from each line of the blast_output_file.
-
-    :param blast_output_file: (file-like object)
-    :return: (sample name, sequence id)
-    """
-    p = re.compile(r'(?P<db>HOT\d+(_\dc?)?_\d+m)_(?P<seq_id>(rep_)?c\d+(_\d)?)')
-    for i, line in enumerate(blast_output_file):
-        row_values = line.strip().split('\t')
-        s = row_values[1]
-        m = p.match(s)
-        if m is None:
-            raise Exception('failed to parse line {}: "{}"'.format(i, line))
-        else:
-            yield s, m.group('db'), m.group('seq_id')
-    print('finished parsing {} rows of BLAST output'.format(i+1))
 
 
 def get_args():
@@ -195,8 +168,8 @@ def get_args():
 
 
 if __name__ == '__main__':
-    #test_blast_output_dp = '/home/jklynch/host/project/muscope/apps/test-blast-output/'
+    #test_blast_output_dp = '/home/jklynch/host/project/muscope/apps/test-blast-output/test.fa-proteins.tab'
     #test_blast_db_fasta_dp = '/home/jklynch/host/project/muscope/ohana/'
-    #test_sequence_output_dp = '/home/jklynch/host/project/muscope/apps/test-sequence_output/'
+    #test_sequence_output_dp = '/home/jklynch/host/project/muscope/apps/test-sequence-output/'
     args = get_args()
     main(**args.__dict__)
