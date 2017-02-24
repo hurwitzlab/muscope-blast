5285d39eb6a28217c3482adfc279dbd2df1d3135
Joshua Lynch <jklynch@email.arizona.edu>
2017-02-24 14:15:11 +0000

    use the development queue for test.sh

--- stampede/test.sh
+++ stampede/test.sh
@@ -3,8 +3,8 @@
 #SBATCH -A iPlant-Collabs
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH -t 02:00:00
-#SBATCH -p normal
+#SBATCH -t 00:30:00
+#SBATCH -p development
 #SBATCH -J mublast
 #SBATCH --mail-type BEGIN,END,FAIL
 #SBATCH --mail-user jklynch@email.arizona.edu
