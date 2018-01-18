e03b8b855e81e1136da1791ff5b7f07e78abe428
Joshua Lynch <jklynch@email.arizona.edu>
2018-01-17 22:37:39 -0500

    updates for stampede2

--- stampede2/test.sh
+++ stampede2/test.sh
@@ -2,22 +2,21 @@
 
 #SBATCH -A iPlant-Collabs
 #SBATCH -N 1
-#SBATCH -n 4
+#SBATCH -n 1
 #SBATCH -t 00:30:00
-#SBATCH -p development
-#SBATCH -J mublast
+#SBATCH -p normal
+#SBATCH -J ohana-blast-test
 #SBATCH --mail-type BEGIN,END,FAIL
 #SBATCH --mail-user jklynch@email.arizona.edu
 
-module load irods
-iget -f /iplant/home/jklynch/data/muscope/blast/test.fa $SCRATCH/muscope-blast
+#module load irods
+#iget -f /iplant/home/jklynch/data/muscope/blast/test.fa $SCRATCH/muscope-blast
 
-ls -l $SCRATCH/muscope-blast
+#ls -l $SCRATCH/muscope-blast
 
-OUT_DIR="$SCRATCH/muscope-blast/test"
-
+OUT_DIR="$SCRATCH/ohana-blast/test"
 if [[ -d $OUT_DIR ]]; then
   rm -rf $OUT_DIR
 fi
 
-run.sh -q "$SCRATCH/muscope-blast/test.fa" -o $OUT_DIR
+run.sh -q test.fa -o $OUT_DIR
